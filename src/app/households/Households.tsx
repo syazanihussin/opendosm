7e93be3147f29ab16ed55d058f7d7b15b8ab4b64
Syazani Bin Hussin <syazanihussin@gmail.com>
2025-11-10 23:19:29 +0800

    Update Households.tsx

--- src/app/households/Households.tsx
+++ src/app/households/Households.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import useFetchIncomes from './hooks/useFetchIncome'
 
 const Households = () => {
+  const { incomes, isLoading, error } = useFetchIncomes()
+
+  useEffect(() => {
+    if (incomes) {
+      console.log('Fetched incomes:', incomes)
+    } else if (error) {
+      console.error('Error fetching incomes:', error)
+    } else if (isLoading) {
+      console.log('Loading incomes...')
+    }
+  }, [incomes, isLoading, error])
+
   return <div>Households</div>
 }
 
