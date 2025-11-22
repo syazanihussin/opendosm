fdc7f1e47abfaaebc3bca76a9b4fe7fd0f45fd69
Syazani Bin Hussin <syazanihussin@gmail.com>
2025-11-22 18:52:08 +0800

    added back components

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { ButtonProps, Variant } from './Button.types'
+
+const Button = ({ label, variant = 'primary' }: ButtonProps) => {
+  const baseClasses = 'px-6 py-2 rounded-lg font-bold'
+
+  const variantClasses: Record<Variant, string> = {
+    primary: 'bg-blue-600 text-white',
+    secondary: 'bg-gray-200 text-black',
+    danger: 'bg-red-600 text-white',
+  }
+
+  return (
+    <>
+      <button className={`${baseClasses} ${variantClasses[variant]}`}>{label}</button>
+    </>
+  )
+}
+
+export default Button
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="bg-white p-6 rounded-xl shadow mb-6 flex justify-between items-center">
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Card
